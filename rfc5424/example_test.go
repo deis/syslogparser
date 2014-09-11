7faf636d776d6348448954ab994b4b8bce1c7e3c
Matthew Fisher <matthewf@opdemand.com>
2014-09-11 08:02:54 -0700

    update fork

--- rfc5424/example_test.go
+++ rfc5424/example_test.go
@@ -2,7 +2,7 @@ package rfc5424_test
 
 import (
 	"fmt"
-	"github.com/jeromer/syslogparser/rfc5424"
+	"github.com/deis/syslogparser/rfc5424"
 )
 
 func ExampleNewParser() {
--- rfc5424/rfc5424_test.go
+++ rfc5424/rfc5424_test.go
@@ -2,7 +2,7 @@ package rfc5424
 
 import (
 	"fmt"
-	"github.com/jeromer/syslogparser"
+	"github.com/deis/syslogparser"
 	. "launchpad.net/gocheck"
 	"testing"
 	"time"
